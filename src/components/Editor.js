d58434d23def2761af68ddbee5f833347b2fb09a
Gabriel <mail@gabrieldavison.me.uk>
2021-03-01 12:38:14 +0000

    tidy up unused imports

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,4 @@
 import { h } from "https://unpkg.com/preact?module";
-import { useRef, useEffect } from "https://cdn.skypack.dev/preact/hooks";
 import htm from "https://unpkg.com/htm?module";
 const html = htm.bind(h);
 
